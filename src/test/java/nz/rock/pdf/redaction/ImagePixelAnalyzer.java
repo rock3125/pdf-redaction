df229b2b4f7a155eaa42416c9927274fc5f53fba
Rock <rock@simsage.ai>
2026-04-10 10:41:48 +1200

    updated files

--- src/test/java/nz/rock/pdf/redaction/ImagePixelAnalyzer.java
+++ src/test/java/nz/rock/pdf/redaction/ImagePixelAnalyzer.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2026 by Rock de Vocht
+ *
+ * All rights reserved. No part of this publication may be reproduced, distributed, or
+ * transmitted in any form or by any means, including photocopying, recording, or other
+ * electronic or mechanical methods, without the prior written permission of the publisher,
+ * except in the case of brief quotations embodied in critical reviews and certain other
+ * noncommercial uses permitted by copyright law.
+ *
+ */
+
 package nz.rock.pdf.redaction;
 
 import org.apache.pdfbox.cos.COSName;
